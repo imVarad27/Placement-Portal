a0e41165864765adcf09d8e32c630de2cd5ef61d
shirish2601 <you@example.com>
2023-03-28 21:03:54 +0530

    added search

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 const buttons = document.querySelectorAll(".dashboard-anchor");
-
+const applicationPage = document.querySelectorAll(".form-student");
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
     buttons.forEach((btn) => {
       btn.classList.remove("active-anchor");
     });
+    applicationPage.forEach((page) => {
+      page.classList.add("inactive-page");
+    });
     if (btn.textContent.trim() === "Logout") {
       window.location.href = "login.html";
-      console.log(btn);
       return;
     }
     btn.classList.remove("active-anchor");
     e.currentTarget.classList.add("active-anchor");
+    if (btn.textContent.trim().includes("Application")) {
+      applicationPage[1].classList.remove("inactive-page");
+      console.log(applicationPage[1]);
+    }
+    if (btn.textContent.trim().includes("Profile")) {
+      applicationPage[0].classList.remove("inactive-page");
+      console.log(applicationPage[1]);
+    }
   });
 });
