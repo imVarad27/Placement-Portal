736f67c827062e65916cbdfea23ba223552feb32
imVarad27 <varadghatage7@gmail.com>
2023-04-04 18:35:15 +0530

    added download button

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-// Set the view engine to EJS
 app.set("view engine", "ejs");
 
 const uri =
@@ -16,8 +15,6 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to MongoDB Atlas", err);
   });
-
-// Define the student schema
 const studentSchema = new mongoose.Schema({
   PRN: String,
   Sname: String,
@@ -27,17 +24,14 @@ const studentSchema = new mongoose.Schema({
   Batch: Number,
 });
 
-// Define the Student model
 const Student = mongoose.model("Student", studentSchema);
 
 app.get("/", async (req, res) => {
   try {
-    // Find all documents in the Student collection
     const students = await Student.find();
 
     console.log("Found students:", students);
 
-    // Render the students view with the data
     res.render("students", { students });
   } catch (error) {
     console.log("Error fetching data from MongoDB Atlas:", error);
